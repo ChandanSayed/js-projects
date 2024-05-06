cbe6d348af10efabea6353a59b3da2442557bccd
ChandanSayed <hangzhou635@gmail.com>
2024-05-07 00:46:18 +0600

    Todo updated

--- todo/js/app.js
+++ todo/js/app.js
@@ -1,18 +1,33 @@
+let todoListContainer = [];
+
 const toDoValue = document.getElementById("todo");
 const toDoList = document.getElementById("todo-list");
 
 function addList(e, t) {
   e.preventDefault();
   console.log(toDoValue.value);
-  // let list;
-  // list.createElement("li");
-  // list.innerText = toDoValue.value;
-  // toDoList.appendChild(list);
-  const html = `<li >${toDoValue.value} <button onclick="deleteList(this)">delete</button> </li>`;
-  toDoList.insertAdjacentHTML("beforeend", html);
+  const id = Date.now();
+  todoListContainer.push({ id: id, value: toDoValue.value });
+
+  toDoList.innerHTML = "";
+  toDoValue.value = "";
+  toDoValue.focus();
+  renderTodoList();
 }
 
-function deleteList(item) {
-  console.log(item.parentElement);
-  item.parentElement.remove();
+function deleteList(id) {
+  const list = todoListContainer.filter(function (todoList) {
+    return todoList.id !== id;
+  });
+
+  todoListContainer = list;
+  toDoList.innerHTML = "";
+  renderTodoList();
+}
+
+function renderTodoList() {
+  todoListContainer.forEach(function (list) {
+    const html = `<li >${list.value} <button onclick="deleteList(${list.id})">delete</button> </li>`;
+    toDoList.insertAdjacentHTML("beforeend", html);
+  });
 }
